0b2ef85acf8ed6dda6561c0c63997638034899b1
Talha rauf <127968337+koderTalha@users.noreply.github.com>
2023-10-14 23:13:37 +0500

    Update key.dart

--- lib/API/key.dart
+++ lib/API/key.dart
@@ -1,2 +1,2 @@
-String Apikey = "AIzaSyCzqur4OJP7pr3kkhFimT_9hNYl7iVgt9w";
+String Apikey = "";
 String baseurl = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=YOUR_API_KEY";
